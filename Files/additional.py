2038bd06eee8b5d1b385123403cea7c48791c132
Karol Ziaja <kziaja34@gmail.com>
2025-04-08 00:54:24 +0200

    added parsing photos & results to tensors

---
+++ Files/additional.py
@@ -0,0 +1,8 @@
+﻿import torch
+
+def hex_to_rgb_tensor(hex_color):
+    hex_color = hex_color.lstrip('#')
+    r = int(hex_color[0:2], 16) / 255.0
+    g = int(hex_color[2:4], 16) / 255.0
+    b = int(hex_color[4:6], 16) / 255.0
+    return torch.tensor([r, g, b])
